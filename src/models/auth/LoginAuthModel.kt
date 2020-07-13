b259f158839d37a0e69ab90c0ea6a2d41563444a
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-07-13 13:07:55 +0200

    Add @JsonProperty annotations to login and sign up auth models

--- src/models/auth/LoginAuthModel.kt
+++ src/models/auth/LoginAuthModel.kt
@@ -1,6 +1,11 @@
 package com.peteralexbizjak.models.auth
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LoginAuthModel(
+    @JsonProperty("email")
     val email: String,
+
+    @JsonProperty("password")
     val password: String
 )
--- src/models/auth/SignUpAuthModel.kt
+++ src/models/auth/SignUpAuthModel.kt
@@ -1,7 +1,14 @@
 package com.peteralexbizjak.models.auth
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class SignUpAuthModel(
+    @JsonProperty("display_name")
     val displayName: String,
+
+    @JsonProperty("email")
     val email: String,
+
+    @JsonProperty("password")
     val password: String
 )
