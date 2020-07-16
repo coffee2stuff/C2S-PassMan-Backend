77efdebdac2b7ded264d4994178f7f80bfe1488b
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-07-16 12:35:08 +0200

    Handle reading configuration file contents from environment variables OR local configuration file

--- src/di/MongoModule.kt
+++ src/di/MongoModule.kt
@@ -8,17 +8,14 @@ import db.repositories.MongoRepo
 import org.koin.dsl.module
 import utils.helpers.retrieveConfigFile
 
-/**
- * Configuration file retrieved as Map.
- */
-private val configFile = retrieveConfigFile()
+
 
 /**
  * Build an instance of a MongoDatabase object.
  */
 private val dbInstance: MongoDatabase = MongoClients
-    .create(configFile["mongo_connection_string"] as String)
-    .getDatabase(configFile["mongo_db_name"] as String)
+    .create(System.getenv("MONGO_CONNECTION_STRING")?.toString() ?: retrieveConfigFile()["mongo_connection_string"] as String)
+    .getDatabase(System.getenv("MONGO_DB_NAME")?.toString() ?: retrieveConfigFile()["mongo_db_name"] as String)
 
 /**
  * Koin module definition. Each component is a singleton (provider and repository).
