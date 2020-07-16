4fa161820d89523cf0b6934c1050b56723e0f2b2
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-07-16 12:14:01 +0200

    Add routes for notes and users

--- src/routes/DatabaseRoute.kt
+++ src/routes/DatabaseRoute.kt
@@ -10,6 +10,8 @@ import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.*
 import models.database.LoginModel
+import models.database.NoteModel
+import models.database.UserModel
 
 fun Route.database(mongoService: MongoService) {
     route("login") {
@@ -65,6 +67,120 @@ fun Route.database(mongoService: MongoService) {
         delete("delete") {
             val request = call.receive<IDQueryModel>()
             val dbOperationResult = mongoService.deleteLoginById(request.documentID)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+    }
+
+    route("note") {
+        post("create") {
+            val request = call.receive<NoteModel>()
+            val dbOperationResult = mongoService.createNote(request)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+
+        get("single") {
+            val queryParameters = call.request.queryParameters
+            val queryParameterID = queryParameters["id"] ?: ""
+            val queryParameterToken = queryParameters["token"] ?: ""
+            if (queryParameterID.isNotBlank() && queryParameterToken.isNotBlank()) {
+                val dbOperationResult = mongoService.fetchNote(queryParameterID, queryParameterToken)
+                call.respond(HttpStatusCode.OK, dbOperationResult)
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorAuthModel(
+                        400,
+                        "400: Bad Request",
+                        "Your request does not contain necessary parameters 'id' and 'token'"
+                    )
+                )
+            }
+        }
+
+        get("all") {
+            val queryParameters = call.request.queryParameters
+            val queryParameterToken = queryParameters["token"] ?: ""
+            if (queryParameterToken.isNotBlank()) {
+                val dbOperationResult = mongoService.fetchAllNotes(queryParameterToken)
+                call.respond(HttpStatusCode.OK, dbOperationResult)
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorAuthModel(
+                        400,
+                        "400: Bad Request",
+                        "Your request does not contain necessary parameter 'token'"
+                    )
+                )
+            }
+        }
+
+        patch("update") {
+            val request = call.receive<NoteModel>()
+            val dbOperationResult = mongoService.updateNote(request)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+
+        delete("delete") {
+            val request = call.receive<IDQueryModel>()
+            val dbOperationResult = mongoService.deleteNoteById(request.documentID)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+    }
+
+    route("user") {
+        post("create") {
+            val request = call.receive<UserModel>()
+            val dbOperationResult = mongoService.createUser(request)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+
+        get("single_by_id") {
+            val queryParameters = call.request.queryParameters
+            val queryParameterID = queryParameters["id"] ?: ""
+            if (queryParameterID.isNotBlank()) {
+                val dbOperationResult = mongoService.fetchUser(true, queryParameterID)
+                call.respond(HttpStatusCode.OK, dbOperationResult)
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorAuthModel(
+                        400,
+                        "400: Bad Request",
+                        "Your request does not contain necessary parameters 'id' and 'token'"
+                    )
+                )
+            }
+        }
+
+        get("single_by_email_password") {
+            val queryParameters = call.request.queryParameters
+            val queryParameterEmail = queryParameters["email"] ?: ""
+            val queryParameterPass = queryParameters["password"] ?: ""
+            if (queryParameterEmail.isNotBlank() && queryParameterPass.isNotBlank()) {
+                val dbOperationResult = mongoService.fetchUser(false, queryParameterEmail, queryParameterPass)
+                call.respond(HttpStatusCode.OK, dbOperationResult)
+            } else {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorAuthModel(
+                        400,
+                        "400: Bad Request",
+                        "Your request does not contain necessary parameters 'id' and 'token'"
+                    )
+                )
+            }
+        }
+
+        patch("update") {
+            val request = call.receive<UserModel>()
+            val dbOperationResult = mongoService.updateUser(request)
+            dbResultCallRespond(call, dbOperationResult)
+        }
+
+        delete("delete") {
+            val request = call.receive<IDQueryModel>()
+            val dbOperationResult = mongoService.deleteUserById(request.documentID)
             dbResultCallRespond(call, dbOperationResult)
         }
     }
